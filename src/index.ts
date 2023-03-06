5c1603480680aa5b1b4fc36c3b25dd93d84a5445
Ismaelleon <Ismaelleon@protonmail.com>
2023-03-06 00:33:43 -0300

    feat: create server and basic html

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+import router from './routes/index';
+import path from 'path';
+
+const app: Express = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+	console.log(`app running on port ${port}`);
+});
